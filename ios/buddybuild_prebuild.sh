70a90dc43b6362b903d171692e688873c673c69b
John <j.cross213@googlemail.com>
2017-04-11 13:49:48 -0700

    more

--- ios/buddybuild_prebuild.sh
+++ ios/buddybuild_prebuild.sh
@@ -17,3 +17,9 @@ echo '=== Install appium 1.4.16'
 npm install -g appium
 echo '=== Install selenium-webdriver'
 npm install wd
+
+echo '=== Authorize simulator access'
+echo password | sudo -S authorize_ios
+
+echo '=== First start appium server, then start your cucumber tests, if one test has failed show exit 1 and mark build as failed'
+nohup appium --native-instruments-lib -lt 999999 &
