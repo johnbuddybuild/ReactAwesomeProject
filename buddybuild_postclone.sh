f6c61a0d3ff9e21418c566c0cda9dbb68a1338b9
John <j.cross213@googlemail.com>
2017-05-26 16:28:17 -0700

    more

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,6 @@
+npm install -g karma
+npm install -g karma-cli
+npm install -g jasmine-core
+npm install -g karma-jasmine
+npm install -g karma-phantomjs-launcher
+npm install -g phantomjs
--- ios/buddybuild_postbuild.sh
+++ ios/buddybuild_postbuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+echo '=== CUSTOM TEST PATH ==='
+export $BUDDYBUILD_CUSTOM_TEST_RESULTS;
 echo '=== FOLDER LISTING POST BUILD'
 ls -1
 
@@ -7,3 +8,41 @@ echo '=== Navigate to tests folder'
 cd ../
 
 #sleep 15 && exec npm test
+echo '=== Folder Listing Post Build'
+echo '-----------------------------'
+ls -1
+echo '-----------------------------'
+
+echo '=== Show required sdk version'
+echo '-----------------------------'
+xcodebuild -showsdks
+echo '-----------------------------'
+
+echo '=== Create a build for iOS simulator'
+xcodebuild -arch i386 -sdk iphonesimulator10.2
+
+echo '=== Show .app path'
+echo '-----------------------------'
+ls /Users/buddybuild/Library/Developer/Xcode/DerivedData/
+ls /Users/buddybuild/Library/Developer/Xcode/DerivedData/TeleTracking/
+ls /Users/buddybuild/Library/Developer/Xcode/DerivedData/TeleTracking/Build/
+ls /Users/buddybuild/Library/Developer/Xcode/DerivedData/TeleTracking/Build/products/
+ls /Users/buddybuild/Library/Developer/Xcode/DerivedData/TeleTracking/Build/products/Debug-iphonesimulator/
+echo '-----------------------------'
+
+echo '=== Install appium'
+npm install -g appium
+
+echo '=== Install selenium-webdriver'
+npm install wd
+
+echo '=== Install protractor'
+npm install -g protractor
+
+echo '=== Authorize iOS on the computer'
+npm install -g authorize-ios
+sudo authorize-ios
+
+echo '=== Start appium server'
+nohup appium &
+sleep 5
